ed9a3a8de1586476558257d0db897fe6aeffaea3
Leo Fabricio <leofabr3@gmail.com>
2022-08-03 10:45:16 -0300

    codeteste implement

--- src/CodeTest.java
+++ src/CodeTest.java
@@ -2,5 +2,19 @@ public class CodeTest {
 
   public static void main(String[] args){
 
+    // Creation of objects
+
+    Employee e = new Employee();
+    Lawyer l = new Lawyer();
+    Secretary s = new Secretary();
+    Marketeer m = new Marketeer();
+    LegalSecretary ls = new LegalSecretary();
+
+    System.out.println(e.getVacationDays());
+    System.out.println(l.getLawyerPosition());
+    System.out.println(s.getVacationDays());
+    System.out.println(m.insideMarket());
+    System.out.println(ls.getVacationDays());
+
   }
 }
